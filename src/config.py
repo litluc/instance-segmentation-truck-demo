fc495c6ba244590c81cc6622601eb82151540d95
litluc <liuguangming11@gmail.com>
2021-04-01 15:18:04 -0400

    adding test data configs

--- src/config.py
+++ src/config.py
@@ -6,10 +6,19 @@ class ProjConfig():
     Project configurations
     """
     project_dir = "/Users/guangmingliu/Work/cv_obj_detection_demo/"
+    # data specifications
     train_image_path = os.path.join(project_dir, 'data/iSAID/val/truck/train')
     train_anno_path = os.path.join(project_dir, 'data/iSAID/val/truck/train_anno.json')
+    train_data_name = 'isaid_truck_train'
+
     val_image_path = os.path.join(project_dir, 'data/iSAID/val/truck/val')
     val_anno_path = os.path.join(project_dir, 'data/iSAID/val/truck/val_anno.json')
+    val_data_name = 'isaid_truck_val'
+
+    test_image_path = val_image_path
+    test_anno_path = val_anno_path
+    test_data_name = val_data_name
+
 
     def __repr__(self):
         rep = f'Project directory: {self.project_dir}'
--- src/data_utils.py
+++ src/data_utils.py
@@ -6,35 +6,40 @@ import detectron2.data.transforms as T
 from config import ProjConfig
 
 
-def register_isaid_truck_data(extra_meta={}, register_val=True):
+def register_isaid_truck_data(extra_meta={}, register_val=True, register_test):
     """
     register project data with name isaid_truck_train/val
     """
     proj_config = ProjConfig()
     extra_meta = extra_meta     # additional metadata to be associated w/ dataset
-    register_coco_instances('isaid_truck_train', 
-                            extra_meta, 
-                            proj_config.train_anno_path, 
+    register_coco_instances(proj_config.train_data_name,
+                            extra_meta,
+                            proj_config.train_anno_path,
                             proj_config.train_image_path)
     if register_val:
-        register_coco_instances('isaid_truck_val', 
-                                extra_meta, 
-                                proj_config.val_anno_path, 
+        register_coco_instances(proj_config.val_data_name,
+                                extra_meta,
+                                proj_config.val_anno_path,
                                 proj_config.val_image_path)
+    if register_test:
+        register_coco_instances(proj_config.test_data_name,
+                                extra_meta,
+                                proj_config.test_anno_path,
+                                proj_config.test_image_path)
 
     return True
 
 
 def get_train_aug():
     """
-    A chain of random data augmentation to be applied at training. 
+    A chain of random data augmentation to be applied at training.
     Parameter values picked to yield realistic images after transformation.
 
     Returns:
         A list of detectron2.data.transforms.Augmentation
     """
     custom_aug = [
-        T.RandomBrightness(.8, 1.2), 
+        T.RandomBrightness(.8, 1.2),
         T.RandomContrast(0.8, 1.2),
         T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
         T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
